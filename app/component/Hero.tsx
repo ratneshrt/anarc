13d6b6effb05d2aad63e9501418c979ada16542d
ratneshrt <ratneshrt19@gmail.com>
2024-11-26 19:04:54 +0530

    landing page done

--- app/component/Hero.tsx
+++ app/component/Hero.tsx
@@ -55,6 +55,7 @@ export function Hero({ anarc, desc1, desc21, desc22 }: { anarc: string; desc1: s
             </div>
           </div>
           <Spotlight className="-top-40 left-0 md:left-60 md:-top-20" fill="blue"></Spotlight>
+          <Spotlight className="-top-40 left-0 md:left-60 md:-top-20" fill="blue"></Spotlight>
       </div>
     </>
   );
